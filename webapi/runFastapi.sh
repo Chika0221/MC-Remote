ca6afb37e648c8ed6cddf8f65cdc2ee653413570
Chika0221 <chikai400up@gmail.com>
2025-12-29 19:31:14 +0900

    スクリプト内のコマンドをコメントアウト

--- webapi/runFastapi.sh
+++ webapi/runFastapi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/chika/Projects/mc_beam/webapi
-/usr/bin/gunicorn main:app -b 0.0.0.0:8000 --reload --workers 1 --threads 4 --log-level 'info' --worker-class 'uvicorn.workers.UvicornWorker'
+# cd /home/chika/Projects/mc_beam/webapi
+# /usr/bin/gunicorn main:app -b 0.0.0.0:8000 --reload --workers 1 --threads 4 --log-level 'info' --worker-class 'uvicorn.workers.UvicornWorker'
