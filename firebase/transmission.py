54575743f996531960ffdf341269e10fe4858fd6
Chika0221 <chikai400up@gmail.com>
2025-12-29 19:48:51 +0900

    Firebase初期化のためのコードを追加し、.gitignoreにファイルを追加

--- firebase/transmission.py
+++ firebase/transmission.py
@@ -1,2 +1,11 @@
 import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+cred = credentials.Certificate("mc-system-1a380-firebase-adminsdk-fbsvc-16af4e4f10.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def on_snapshot(col_snapshot, changes, read_time):
+    pass
